6dfc0e50838d271ac7c3e78d9d89f88f28b8c832
fadil <fadil.alfarhan@gmail.com>
2023-11-01 09:04:09 +0100

    Deconstructing tuples

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -73,19 +73,22 @@ WriteLine(format:
     arg1: gunny.HomePlanet,
     arg2: gunny.Instantiated);
     
-    bob.WriteToConsole();
-    WriteLine(bob.GetOrigin());
-    
-    (string, int) fruit = bob.GetFruit();
-    WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
-    
-    var fruitNamed = bob.GetNamedFruit();
-    WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}");
+bob.WriteToConsole();
+WriteLine(bob.GetOrigin());
+
+(string, int) fruit = bob.GetFruit();
+WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+var fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}");
     
 var thing1 = ("Neville", 4);
 WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
 
 var thing2 = (bob.Name, bob.Children.Count);
 WriteLine($"{thing2.Item1} has {thing2.Item2} children.");
+
+(string fruitName, int fruitNumber) = bob.GetFruit();
+WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
     
     
