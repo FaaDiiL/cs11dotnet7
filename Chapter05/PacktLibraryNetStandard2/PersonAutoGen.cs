88c85fe81a53f3824e7c5d2ff8982343e728662f
fadil <fadil.alfarhan@gmail.com>
2023-11-06 20:49:33 +0100

    Defining settable properties

--- Chapter05/PacktLibraryNetStandard2/PersonAutoGen.cs
+++ Chapter05/PacktLibraryNetStandard2/PersonAutoGen.cs
@@ -22,4 +22,32 @@ public partial class Person
         if (birthdate.Date > today.AddYears(-age)) age--;
         return age;
     }
+    
+    // a read-write property defined using C# 3.0 syntax
+    public string? FavoriteIceCream { get; set; } // auto-syntax
+    // a private field to store the property value
+    private string? favoritePrimaryColor;
+    // a public property to read and write to the field
+    public string? FavoritePrimaryColor
+    {
+        get
+        {
+            return favoritePrimaryColor;
+        }
+        set
+        {
+            switch (value?.ToLower())
+            {
+                case "red":
+                case "green":
+                case "blue":
+                    favoritePrimaryColor = value;
+                    break;
+                default:
+                    throw new ArgumentException(
+                        $"{value} is not a primary color. " +
+                        "Choose from: red, green, blue.");
+            }
+        }
+    }
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -130,3 +130,17 @@ WriteLine(sam.Origin);
 WriteLine(sam.Greeting);
 WriteLine(sam.Age);
 
+sam.FavoriteIceCream = "Chocolate Fudge";
+WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}.");
+string color = "Red";
+try
+{
+    sam.FavoritePrimaryColor = color;
+    WriteLine($"Sam's favorite primary color is {sam.
+        FavoritePrimaryColor}.");
+}
+catch (Exception ex)
+{
+    WriteLine("Tried to set {0} to '{1}': {2}",
+        nameof(sam.FavoritePrimaryColor), color, ex.Message);
+}
